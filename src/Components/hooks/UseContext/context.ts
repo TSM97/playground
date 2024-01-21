aa0bcc2430e250c79ef0e3dc1b1be1b4c09edebb
TSM97 <Gtsamadias@hotmail.com>
2024-01-21 20:37:49 +0200

    import fix

--- src/Components/hooks/UseContext/context.ts
+++ src/Components/hooks/UseContext/context.ts
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react";
-import { User } from "../../App";
+import { User } from "../../../App";
 
 export const DashboardContext = createContext<User | undefined>(undefined);
 
